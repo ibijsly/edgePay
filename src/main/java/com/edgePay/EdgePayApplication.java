ad3745da06e2a291b9e9019b720ad6f021ac1648
Ibijola <ibijola@cashenvoy.com>
2018-07-20 17:14:40 +0100

    initial commit

---
+++ src/main/java/com/edgePay/EdgePayApplication.java
@@ -0,0 +1,12 @@
+package com.edgePay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgePayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgePayApplication.class, args);
+	}
+}
