f6c35aae338098781d3c314c83de1df7e8a8d3d4
Joshua Okobia <joshuaokobia@Joshuas-MacBook-Pro.local>
2018-07-20 19:31:35 +0100

    Fixed issue with Identification Type Repository

--- src/main/java/com/edgePay/repository/IdentificationTypeRepository.java
+++ src/main/java/com/edgePay/repository/IdentificationTypeRepository.java
@@ -1,10 +1,9 @@
 package com.edgePay.repository;
 
+import com.edgePay.model.IdentificationType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IdentificationTypeRepository extends JpaRepository<IdentificationTypeRepository, Long>
-{
-
+public interface IdentificationTypeRepository extends JpaRepository<IdentificationType, Long> {
 }
--- src/main/java/com/edgePay/service/VerificationService.java
+++ src/main/java/com/edgePay/service/VerificationService.java
@@ -1,5 +1,7 @@
 package com.edgePay.service;
 
+import org.springframework.stereotype.Service;
 
+@Service
 public class VerificationService {
 }
